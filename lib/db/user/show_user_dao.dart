dc0ac44fc871292e6cc094eb3af33d00ec389055
maichu <chuthimai18245@gmail.com>
2024-05-07 23:13:19 +0700

    chay user ra man hinh admin user

--- lib/db/user/show_user_dao.dart
+++ lib/db/user/show_user_dao.dart
@@ -2,12 +2,12 @@ import 'package:chat_app_ttcs/models/user/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ShowUserDAO {
-  final _db = FirebaseFirestore.instance.collection('Users');
+  final _db = FirebaseFirestore.instance;
 
   ShowUserDAO();
 
   Future<List<User>> getAllUsers() async{
-    final users = await _db.get().then((value) {
+    final users = await _db.collection('Users').get().then((value) {
       return value.docs.map((e) {
         return User.toUser(e.data());
       });
@@ -15,4 +15,6 @@ class ShowUserDAO {
 
     return users.toList();
   }
+
+
 }
